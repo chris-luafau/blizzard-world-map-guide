b7ff654701ad2a9bd530e2de61d9118e91c94eba
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2020-01-22 16:39:49 -0800

    Updates

---
+++ precache-manifest.33fb0ae006d162cdaed61c28a4b04851.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce49d6eb459c4e5c950fba476439906e",
+    "url": "/blizzard-world-map-guide/index.html"
+  },
+  {
+    "revision": "3d33bc377cc4fce80bec",
+    "url": "/blizzard-world-map-guide/static/js/2.203aa472.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/blizzard-world-map-guide/static/js/2.203aa472.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3650ed9d3f6af4010698",
+    "url": "/blizzard-world-map-guide/static/js/main.1a4ca8ad.chunk.js"
+  },
+  {
+    "revision": "ad9485840bafe13469e6",
+    "url": "/blizzard-world-map-guide/static/js/runtime-main.1ca8136a.js"
+  },
+  {
+    "revision": "54f32a5332cf4128b3b7e7abfebed0bd",
+    "url": "/blizzard-world-map-guide/static/media/bignoodletoo.54f32a53.ttf"
+  },
+  {
+    "revision": "549acd0b679125aa20e211c11b23aa78",
+    "url": "/blizzard-world-map-guide/static/media/blizz-background-3.549acd0b.jpg"
+  },
+  {
+    "revision": "22945cd970853fac6a2df067092b6e37",
+    "url": "/blizzard-world-map-guide/static/media/blizz-world-center.22945cd9.jpg"
+  },
+  {
+    "revision": "e029240ff5d46a351ebe76d4b1789ef4",
+    "url": "/blizzard-world-map-guide/static/media/blizz-world-cover1.e029240f.png"
+  },
+  {
+    "revision": "d25d40f019a780844ea3715765d5b2bc",
+    "url": "/blizzard-world-map-guide/static/media/blizz-world-cover2.d25d40f0.png"
+  },
+  {
+    "revision": "5c8ccab2cb8463ddeb5257a6bfbddd9f",
+    "url": "/blizzard-world-map-guide/static/media/blizz-world-left.5c8ccab2.jpg"
+  },
+  {
+    "revision": "fd5c9799ef6369438d5e519cc9a0a7f1",
+    "url": "/blizzard-world-map-guide/static/media/blizz-world-right.fd5c9799.jpg"
+  },
+  {
+    "revision": "90cecd05a2a4f7ff7b33d49ed9228288",
+    "url": "/blizzard-world-map-guide/static/media/blizzard-world-map.90cecd05.jpg"
+  },
+  {
+    "revision": "946fcedecbdeb44079542a8a630eaa68",
+    "url": "/blizzard-world-map-guide/static/media/diablo-logo.946fcede.png"
+  },
+  {
+    "revision": "cdc2d51693665b073ee233d96e5ba133",
+    "url": "/blizzard-world-map-guide/static/media/hs-logo.cdc2d516.png"
+  },
+  {
+    "revision": "53fd6670dea280fc6ea6305c37e1c1b8",
+    "url": "/blizzard-world-map-guide/static/media/sc-logo.53fd6670.png"
+  },
+  {
+    "revision": "afc95418158ec22a2967e1c6a674eafb",
+    "url": "/blizzard-world-map-guide/static/media/wow-logo.afc95418.png"
+  }
+]);
